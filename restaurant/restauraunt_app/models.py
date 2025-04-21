6e52c2584b2e46ada3460b9a4505f9491852512f
Pilot2281 <alexulanov1242004@gmail.com>
2025-04-21 22:38:19 +0400

    add models

--- restaurant/restauraunt_app/models.py
+++ restaurant/restauraunt_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -6,3 +7,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class User(models.Model):
+    login = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)  # Для хранения хэша пароля
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def __str__(self):
+        return self.login
