976f3517196faefe072de20eee4908f04632a9c5
Pilot2281 <alexulanov1242004@gmail.com>
2025-04-21 22:35:39 +0400

    add urls

--- restaurant/restauraunt_app/urls.py
+++ restaurant/restauraunt_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, about_us, menu, personal_page, login_page, logout
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about_us, name='about_us'),
+    path('menu/', menu, name='menu'),
+    path('personal/', personal_page, name='personal_page'),
+    path('login/', login_page, name='login_page'),
+    path('logout/', logout, name='logout'),
 ]
