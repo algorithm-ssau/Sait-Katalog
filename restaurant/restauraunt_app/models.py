1746c1d4ced4d716ae058df5ceba9bf458e1d606
Alexander Pechinkin <pechinkin04@yandex.ru>
2025-05-08 01:12:55 +0400

    edit models

--- restaurant/restauraunt_app/models.py
+++ restaurant/restauraunt_app/models.py
@@ -20,3 +20,25 @@ class User(models.Model):
 
     def __str__(self):
         return self.login
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='dishes/')  # <--- Важно!
+
+    def __str__(self):
+        return self.name
+
+class FavoriteDish(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorite_dishes')
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'dish')  # Предотвращает дублирование записей
+
+    def __str__(self):
+        return f"{self.user.login} - {self.dish.name}"
