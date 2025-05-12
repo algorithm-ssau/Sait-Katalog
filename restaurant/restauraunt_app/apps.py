78b1b30c62254f29593d228291d93a61ed975300
lafteraf <143380328+lafteraf@users.noreply.github.com>
2025-05-12 23:29:41 +0400

    Add files via upload

--- restaurant/restauraunt_app/apps.py
+++ restaurant/restauraunt_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RestaurauntAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "restauraunt_app"
+
+    def ready(self):
+        import restauraunt_app.signals
