e20b5fb0ce0198620a559f5672c64acc80e2e445
lafteraf <143380328+lafteraf@users.noreply.github.com>
2025-05-14 03:00:45 +0400

    Update settings.py

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'restauraunt_app/static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'restaurant/restauraunt_app/static'),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 💥 Обязательно для Render!
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'restauraunt_app/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'restaurant/restauraunt_app/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
